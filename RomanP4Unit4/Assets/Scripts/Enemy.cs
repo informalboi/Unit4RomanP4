059f5fb0e704f1452000ba53842e24410a04597f
informalboi <733416@student.dusd.net>
2026-04-22 11:43:43 -0700

    vgb nm

--- RomanP4Unit4/Assets/Scripts/Enemy.cs
+++ RomanP4Unit4/Assets/Scripts/Enemy.cs
@@ -1,19 +1,16 @@
 using UnityEngine;
 
-
-public class RotateCamera : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    public float rotationSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        
     }
 }
